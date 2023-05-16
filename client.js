219c7c66fcc0fe43386409fa63824432c3682196
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2023-05-16 15:07:01 +0200

    add client

---
+++ client.js
@@ -0,0 +1,38 @@
+const fs = require('fs/promises');
+
+//const SERVER = 'https://genomogram.richard-kammermeier.ch';
+// const SERVER = 'http://tower:2023';
+const SERVER = 'http://localhost:3000';
+const GENOME = 'Human';
+const PATH = './';
+
+async function getData(genome, SERVER) {
+  const startTime = Date.now();
+  const dataToSend = genome;
+
+  const response = await fetch(`${SERVER}/process-data`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ data: dataToSend }),
+  });
+  // console.log(await response.text());
+  const processedData = await response.json();
+  const endTime = Date.now();
+  console.log(`Time taken: ${Math.floor((endTime - startTime) / 1000)}s`);
+  return processedData;
+}
+
+getData(GENOME, SERVER)
+  .then((response) => {
+    const data = response.data;
+    if (data) {
+      return fs.writeFile(
+        `${PATH}${GENOME.toLowerCase()}.json`,
+        JSON.stringify(data, null, 2)
+      );
+    } else {
+      throw new Error('No data received from the server');
+    }
+  })
+  .then(() => console.log(`Data written to ${GENOME.toLowerCase()}.json`))
+  .catch((error) => console.error('Error:', error));
